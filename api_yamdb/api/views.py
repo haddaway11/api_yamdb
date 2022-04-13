1664d49bb736b57f54b0e2a7a2e7c3a915e3f930
Svetlana Ponomareva <s-ponomareva@mail.ru>
2022-04-13 16:09:23 +0500

    модель user частично

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,14 @@
 from rest_framework import permissions, viewsets
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status, viewsets
 from reviews.models import User, Category, Genre, Title, Review, Comment
-
-
+from django.core.mail import send_mail
+from .serializers import (SignUpSerializer, TokenSerializer,
+                          UserSerializer, TitleSerializer,
+                          NoStaffSerializer, CategorySerializer,
+                          GenreSerializer, ReviewSerializer,
+                          CommentSerializer)
 
 
 class APIToken(APIView):
@@ -10,10 +16,30 @@ class APIToken(APIView):
 
 
 class APISignup(APIView):
-    pass
+    permission_classes = (permissions.AllowAny,)
 
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            send_mail(
+                'Тема письма',
+                'Текст письма.',
+                'from@example.com',  # Это поле "От кого"
+                ['to@example.com'],  # Это поле "Кому" (можно указать список адресов)
+                fail_silently=False, # Сообщать об ошибках («молчать ли об ошибках?»)
+            ) 
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class UserViewSet(viewsets.ModelViewSet):
+    # queryset = User.objects.all()
+    # serializer_class = UserSerializer
+    # permission_classes = ()
+    # filter_backends = ()
+    # search_fields = ('username', )
+
+    
+    # return Response(serializer.data)
     pass
 
 
