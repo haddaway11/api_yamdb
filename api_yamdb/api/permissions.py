19254e8c77499bbe8a3501f9c464dadc4a3555ca
Svetlana Ponomareva <s-ponomareva@mail.ru>
2022-04-13 18:53:07 +0500

    мерж2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,21 +5,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     message = 'Изменение чужого контента запрещено!'
 
     def has_object_permission(self, request, view, obj):
-<<<<<<< HEAD
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    message = 'Действие доступно только администратору!'
-
-    def has_permission(self, request, view):
-        return (
-            request.user in permissions.SAFE_METHODS
-            or request.user.is_staff or request.user.role == 'admin'
-        )
-=======
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
 
@@ -44,4 +29,3 @@ class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
->>>>>>> feauture/auth-users
