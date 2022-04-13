b3fb908bb4ce3ed98063f83d7450913fcd345379
Юрий Юрчак <pontonovanton@gmail.com>
2022-04-13 15:03:24 +0300

    заработали тесты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,22 +8,22 @@ from .views import (
 
 
 router = routers.DefaultRouter()
-# router.register('users', UserViewSet)
-# router.register('categories', CategoryViewSet)
-# router.register('genres', GenreViewSet)
-# router.register('titles', TitleViewSet)
-# router.register(
-#     r'titles/(?P<title_id>\d+)/reviews',
-#     ReviewViewSet, basename='review'
-# )
-# router.register(
-#     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-#     CommentViewSet, basename='comments'
-# )
+router.register('users', UserViewSet)
+router.register('categories', CategoryViewSet)
+router.register('genres', GenreViewSet)
+router.register('titles', TitleViewSet)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet, basename='review'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments'
+)
 
 urlpatterns = [
     path('v1/auth/signup/', APISignup.as_view(), name='signup'),
     path('v1/auth/token/', APIToken.as_view(), name='token'),
-    # path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(router.urls)),
 ]
