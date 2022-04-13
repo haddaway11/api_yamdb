48e43c7d15ca78e71fd52ca5ed0bdda7103d0da3
Svetlana Ponomareva <s-ponomareva@mail.ru>
2022-04-13 18:43:17 +0500

    пермишены

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,6 +5,27 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     message = 'Изменение чужого контента запрещено!'
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
+
+
+class IsModeratorOwnerAdminOrReadOnly(permissions.BasePermission):
+   
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin)
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+   
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_admin)
+
+
+class ReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -24,19 +24,57 @@ class User(AbstractUser):
         default='user',
         blank=True,
     )
+    confirmation_code = models.CharField(
+        'Код подтверждения',
+        max_length=150,
+        null=True,
+        blank=False,
+    )
+    
+    def is_admin(self):
+        return self.role == ADMIN
 
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    def is_user(self):
+        return self.role == USER
+
+    def __str__(self):
+        return self.username
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    slug = slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    slug = slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Title(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='Genre', blank=True, null=True
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='Category', blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
