c2bdc4d822ea5786ee8f30253b0b2ba383d0a108
Svetlana Ponomareva <s-ponomareva@mail.ru>
2022-04-13 14:07:30 +0500

    миграции

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,18 +8,18 @@ from .views import (
 
 
 router = routers.DefaultRouter()
-router.register('users', UserViewSet)
-router.register('categories', CategoryViewSet)
-router.register('genres', GenreViewSet)
-router.register('titles', TitleViewSet)
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet, basename='review'
-)
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet, basename='comments'
-)
+# router.register('users', UserViewSet)
+# router.register('categories', CategoryViewSet)
+# router.register('genres', GenreViewSet)
+# router.register('titles', TitleViewSet)
+# router.register(
+#     r'titles/(?P<title_id>\d+)/reviews',
+#     ReviewViewSet, basename='review'
+# )
+# router.register(
+#     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+#     CommentViewSet, basename='comments'
+# )
 
 urlpatterns = [
     path('v1/auth/signup/', APISignup.as_view(), name='signup'),
