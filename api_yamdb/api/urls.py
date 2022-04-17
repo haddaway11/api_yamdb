c3abd507e95d3ffd5dd009387ec021cb20ad8ebc
Иван Коваль <ivankoval.work@yandex.ru>
2022-04-18 02:13:48 +0500

    urls

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, ReviewViewSet
+
+router = DefaultRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
+    basename='Reviews'
+)
+router.register(
+    (
+        r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments'
+    ), CommentViewSet, basename='Comments'
+)
+
+urlpatterns = [
+    # path('v1/', include('djoser.urls')),
+    # path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls)),
+]
